8a5cd3aff248d149a16bfd9bf2f8326331d03c7f
french-femboi <cat@french-femboi.eu>
2024-10-01 17:44:06 +0200

    Add .npmignore files to isexe and tailwindcss modules

---
+++ index.php
@@ -0,0 +1,128 @@
+<!--
+                                                                                            
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠏⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣾⣿⢐⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⢸⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⡇⣿⡇⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⡙⣿⣶⣦⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⢿⣯⣿⣻⣽⣾⢿⡇⣿⣷⣝⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⣎⠻⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⣀⣤⣤⢶⣶⣶⡾⣶⢷⡶⣦⣴⢿⣳⢿⣻⣟⡾⡿⣽⡾⣿⢃⣿⣿⣿⣿⣶⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⡏⣷⣘⠻⣿⣿⣿⡿⣿⣿⢿⡿⣟⣿⡷⣶⡴⣞⣿⣻⣽⡾⣿⣳⢯⡟⠿⢯⣿⡽⣯⣿⣻⣟⢿⡾⣽⣻⢷⣻⠏⡼⣿⣿⣿⡻⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀     This code was made by Cat
+⠀⠀⠀⠀⠀⠀⣿⠹⣇⢷⣌⡻⢾⣟⣿⢾⣟⣿⣻⢯⣟⣯⣟⣯⢷⣟⣷⣻⢷⣯⣟⡿⣳⢶⣌⡙⣷⣳⢟⡾⣏⣟⡷⣯⣻⠃⣼⣿⣮⢛⣥⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣇⢿⡼⣿⣿⣜⡻⢿⣿⡼⣟⡿⣟⣿⡼⣻⡼⣟⣻⣼⣻⢿⣸⡟⣿⣻⢟⣼⣻⢧⡛⠿⣼⢻⡟⣿⣣⢟⣿⣤⣛⢿⣇⢿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        This is Felix, I didn't make him...
+⠀⠀⠀⠀⠀⠀⠀⢻⠜⣵⣿⣿⣿⣿⣶⣭⣙⠿⠽⢻⣎⡿⣝⣯⣛⠗⣠⢿⣹⢧⣟⢧⣻⢮⣳⢏⣯⢻⣤⠩⡳⣟⣳⢿⡰⣎⠻⡽⠷⠍⠂⠀⣤⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⡿⢟⣻⣭⣿⣿⡏⡟⣠⣞⣧⣟⢾⣱⢏⢂⣯⣟⣧⣟⣮⡟⣞⣳⡽⣞⣭⢿⣜⣆⢱⡈⢽⣫⣷⣜⣃⡇⢿⣷⡄⣮⢿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀         he's from an anime or comic, I'm not even sure...
+⠀⠀⠀⠀⠀⠀⠘⠻⠭⠷⢾⡿⠿⢹⣿⣿⡏⣰⣏⡞⣶⡹⣾⡹⣞⠸⣞⣿⢻⣼⢶⣻⡭⢷⣹⢾⣹⢮⢷⣺⠀⣷⡈⢗⡾⣹⢯⣿⡘⣿⣿⡞⢸⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠲⡘⣿⡜⣿⣿⢡⡿⣼⣹⢎⡷⢧⣛⡼⠀⣟⠿⣞⡻⢮⡳⣝⢯⡞⣧⣛⠾⢣⠛⠀⢷⣳⡈⢾⡱⣟⠶⣅⢻⣿⡏⣄⢝⣻⠇⣀⣀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣦⣝⠊⢷⡹⢏⣤⣶⢸⢧⢯⣝⡳⣏⡗⣠⢫⡻⣵⢻⣭⢳⣛⡮⡽⢶⡁⠀⢆⠀⡘⣦⠳⣳⠈⢷⣭⣛⡟⣦⡙⠣⣿⣦⠙⠿⣿⡋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⣰⣰⣿⣿⡟⡼⣫⡞⣮⢳⣝⢠⣿⠆⠁⠠⠀⣯⠳⣝⢾⡹⢧⡻⠄⡆⣠⣶⣿⣷⣜⢧⠘⡶⣹⢞⡵⣻⠀⣿⢟⠷⠄⠈⠥⣄⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣇⠇⣿⣿⣿⠀⡷⣭⢻⡜⠘⡎⣼⣿⣘⣀⡁⠈⠷⣣⠘⢧⣛⢧⣛⠀⣴⣟⣿⡿⠻⠟⢃⣀⢳⡹⣎⡷⣹⠀⠘⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣀⣠⣼⣿⢿⡅⣶⡽⣿⣿⢐⡟⣼⢣⡟⠀⣼⣿⣿⡿⣿⢿⣷⣦⣌⡓⠌⢪⣗⠃⣼⣯⡾⠋⡁⠀⠀⢦⡙⠀⣳⡝⣾⡱⠀⠀⠀⠘⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⣷⠗⠋⣼⣿⣿⠈⣃⠸⡜⣧⢻⡜⠆⣿⠟⠋⠁⠙⠓⠾⣿⣿⣿⣿⣦⣩⣾⣿⣿⣡⡀⠀⠀⠀⢸⡇⡆⢶⡹⢶⡙⢰⢣⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⠊⠉⠈⡻⠀⠽⠀⡝⡶⣋⠾⡁⢀⣶⠀⠃⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣰⣉⣼⣿⢡⢏⡳⢏⠆⢮⡝⢧⣀⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⠀⠀⠀⢸⠁⠀⠸⠆⢩⢳⣍⡻⣱⠈⢻⡄⠀⢀⢠⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣾⣿⡏⠼⢮⣙⠃⡰⢧⢞⡣⢏⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡤⣄⠀⠀⣟⡀⢳⡌⢷⣩⢗⡸⣿⣶⣾⣒⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⡄⣸⠓⢠⡚⣭⢳⡋⠀⡍⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣬⢛⠼⣎⡵⣢⠙⠎⣖⠦⡑⠘⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣠⣴⠇⠆⠙⠖⡣⠁⠀⢀⣀⣤⣤⣶⣤⣴⣤⣤⣤⣤
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡴⢫⠘⢦⢳⡱⢫⢖⡈⢉⡓⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠈⠘⠀⠀⠀⣀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣣⠆⠀⠣⢝⡹⢎⡽⣢⠹⡟⠛⠉⠉⠙⣿⣿⣿⣿⣿⣿⢿⣿⣟⣻⣿⣿⣿⣿⣿⠏⠀⠀⠄⠀⠀⢰⣿⣷⣝⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⡄⠀⠀⠉⢩⣴⣶⣿⣧⠀⠀⠀⠀⣿⣿⣿⣿⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠀⠀⠀⢸⣿⣿⣧⣿⣤⣤⡄⠀⢠⣾⣿⣿⡟
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢀⣤⣾⣿⣿⣿⣿⣿⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⢹⣿⣤⠀⢠⡤⣿⣿⣿⣿⣷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣍⣛⠻⠿⣿⣿⣿⣿⡿⢋⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡿⣸⣿⣿⣿⣶⣠⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠐⠊⠕⠓⠒⠒⠂⢀⡈⠡⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⡇⡿⢿⣿⣿⣿⣿⣿⣿⣿⡇⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠈⠀⠀⠀⠈⢓⢨⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠁⠀⠀⣿⣿⣿⣿⣿⣿⣿⣻⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⡜⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠏⠀⠀⠀⠿⣿⣿⣿⣿⣿⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣀⣿⣿⣿⡿⠿⠫⠼⣿⣿⣿⣿⣿⡿⣿⣿⣿⣆⠀⠀⠀⡀⠀⠀⠀⠀⢸⢡⠀⠀⠀⠀⠀⠀⠀⣶⣤⣼⣿⣿⣿⣶⣶⣶⣶⣦⣤⣬⠛⣋⣉⠿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠉⠀⠀⠀⠀⢀⣀⣠⣼⣿⣿⣿⣿⡿⣆⣹⣯⣭⣕⢺⣿⣷⡀⠀⠀⠀⠀⠛⡄⠀⠀⠀⠀⠀⣼⣿⣿⡿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢩⣽⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣴⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⣣⣿⣿⣿⣿⣿⣿⣷⣝⢿⣷⡀⠀⠀⠀⢠⢁⠀⠀⠀⠀⣰⣿⡿⠫⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣎⢿⣿⡿
+            
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Femrise ASCII</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&amp;display=swap" rel="stylesheet">
+  <link href="src/styles/output.css" rel="stylesheet">
+  <link rel="icon" href="favicon.png">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="description" content="Get some ASCII art to use in your code! Mostly Boykisser and some other random stuff...">
+  <meta name="author" content="Catpawz">
+  <meta content="#A443D1" data-react-helmet="true" name="theme-color">
+  <meta property="og:image" content="favicon.png">
+
+</head>
+
+<body class="bg-gray-900">
+
+  <div class="relative isolate overflow-hidden bg-gray-900">
+    <svg class="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" aria-hidden="true">
+      <defs>
+        <pattern id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+          <path d="M.5 200V.5H200" fill="none" />
+        </pattern>
+      </defs>
+      <svg x="50%" y="-1" class="overflow-visible fill-gray-800/20">
+        <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
+      </svg>
+      <rect width="100%" height="100%" stroke-width="0" fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" />
+    </svg>
+    <div class="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]" aria-hidden="true">
+      <div class="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#A443D1] to-[#46dae5] opacity-20" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"></div>
+    </div>
+    <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+      <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+        <h1 class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl flex items-center">
+          ASCII art for your code
+        </h1>
+        <p class="mt-6 text-lg leading-8 text-gray-300">This page displays my activity on several different platform, it refreshes itself automatically for some items.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-auto px-6 py-10">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6" id="cards-container">
+      <!-- Cards will be dynamically inserted here -->
+    </div>
+  </div>
+
+
+
+
+  <?php include 'inc/footer.php'; ?>
+</body>
+<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+<script>
+  kofiWidgetOverlay.draw('french_femboi', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#794bc4',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
+</script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+      fetch('art.json')
+        .then(response => response.json())
+        .then(cards => {
+          const container = document.getElementById('cards-container');
+          cards.forEach(card => {
+            const cardElement = document.createElement('div');
+            cardElement.className = 'bg-gray-800 rounded-lg shadow-lg p-6';
+            cardElement.innerHTML = `
+              <h3 class="text-xl font-bold text-white mb-2">${card.title}</h3>
+              <p class="text-gray-300 mb-4">${card.description}</p>
+              <pre class="text-gray-400">${card.ascii_art}</pre>
+            `;
+            container.appendChild(cardElement);
+          });
+        })
+        .catch(error => {
+          console.error('Error loading cards:', error);
+        });
+    });
+  </script>
+</html>
