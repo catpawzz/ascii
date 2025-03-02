2dc32d3dbe5791127b070cdf8ca6d644e58db1c1
catpawz <cat@catpawz.net>
2025-03-02 12:34:00 +0100

    Update GitHub link in footer to reflect new repository URL

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,7 @@
             <div class="max-w-3xl mx-auto">
                     <ul class="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 mb-10">
                         <li><a href="https://catpawz.net" class="text-purple-200 hover:text-purple-300">Main website</a></li>
-                        <li><a href="https://github.com/french-femboi/ascii" class="text-purple-200 hover:text-purple-300">GitHub</a></li>
+                        <li><a href="https://github.com/catpawzz/ascii" class="text-purple-200 hover:text-purple-300">GitHub</a></li>
                         <li><a href="/generator" class="text-purple-200 hover:text-purple-300">Generator</a></li>
                     </ul>
                     <span class="text-lg text-gray-100 text-center block">©<a href="https://catpawz.net/">French Femboi</a> 2024 - Present, All rights reserved.</span>
